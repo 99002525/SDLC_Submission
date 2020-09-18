59fc5554ef2a0a87631d44be0d17c1f4b209aee7
99002525 <71481208+99002525@users.noreply.github.com>
2020-09-18 17:39:18 +0530

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int calculator_operand1 = 0;
 int calculator_operand2 = 0;
 
 /* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, SQUARE, POWER, EXIT };
+enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, SQUARE, POWER, PRIME, EXIT };
 
 /* Display the menu of operations supported */
 void calculator_menu(void);
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
 void calculator_menu(void)
 {
     printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Square\n6. Power\n7. Exit");
+    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Square\n6. Power\n7. Prime\n8. Exit");
     printf("\n\tEnter your choice\n");
    
      __fpurge(stdin);
@@ -111,6 +111,14 @@ void calculator_menu(void)
             calculator_operand1, 
             calculator_operand2,
             power(calculator_operand1, calculator_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+         case PRIME:
+            printf("\n\t %d is a prime number if %d is 1 \nEnter to continue", 
+            calculator_operand1, 
+            prime(calculator_operand1));
             
             __fpurge(stdin);
             getchar();
