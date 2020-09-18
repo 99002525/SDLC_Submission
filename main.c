dcebf020f65951e7a3c341cfed57c29d4172b6f5
99002525 <71481208+99002525@users.noreply.github.com>
2020-09-18 17:18:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <calculator_operations.h>
+#include <math.h>
 
 /* Status of the operation requested */
 #define VALID   (1)
@@ -12,7 +13,7 @@ int calculator_operand1 = 0;
 int calculator_operand2 = 0;
 
 /* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, SQUARE, EXIT };
+enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, SQUARE, POWER, EXIT };
 
 /* Display the menu of operations supported */
 void calculator_menu(void);
@@ -33,7 +34,7 @@ int main(int argc, char *argv[])
 void calculator_menu(void)
 {
     printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Square\n6. Exit");
+    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Square\n6. Power\n7. Exit");
     printf("\n\tEnter your choice\n");
    
      __fpurge(stdin);
@@ -105,7 +106,16 @@ void calculator_menu(void)
             __fpurge(stdin);
             getchar();
             break;
-        case 6:
+         case POWER:
+            printf("\n\t%d ^ %d = %d\nEnter to continue", 
+            calculator_operand1, 
+            calculator_operand2,
+            power(calculator_operand1, calculator_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+        case 7:
             exit(0);
             break;
         default:
