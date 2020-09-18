ecb7a1e41c8950b13c38b59141aacae7ef04c643
99002525 <71481208+99002525@users.noreply.github.com>
2020-09-18 17:29:39 +0530

    Update calculator_operations.c

--- src/calculator_operations.c
+++ src/calculator_operations.c
@@ -38,3 +38,25 @@ int power(int operand1, int operand2)
     else
         return pow(operand1, operand2);
 }
+
+int prime(int num)
+{
+    /* return -1 if a negative number*/
+    int i,count=0;
+    if (num<0)
+        return -1;
+
+    for(i=1;i<=num;i++)
+    {
+        if((num%i)==0)
+        {
+            count++;
+        }
+    }
+    /*return 1 for a prime number*/
+    if(count==2)
+        return 1;
+    /*return 0 for a non prime number*/
+    else
+        return 0;
+}
